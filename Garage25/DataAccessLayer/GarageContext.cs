2f9ee0fe88bc7a52e881b6bd73bec2041412c0c1
Lars Börlin <lars.borlin@gmail.com>
2017-11-02 13:27:56 +0100

    Database created

---
+++ Garage25/DataAccessLayer/GarageContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Garage25.Models;
+
+namespace Garage25.DataAccessLayer
+{
+    public class GarageContext : DbContext
+    {
+        public GarageContext()
+            : base ("GarageContext")
+        {
+        }
+        
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<ParkedVehicle> ParkedVehicls { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<VehicleType> VechicleTypes { get; set; }
+    }
+}
---
+++ Garage25/Migrations/201711021218599_Initial.cs
@@ -0,0 +1,65 @@
+namespace Garage25.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Colors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ParkedVehicles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        RegistrationNumber = c.String(nullable: false),
+                        VehicleBrand = c.String(),
+                        InDate = c.DateTime(nullable: false),
+                        ColorId = c.Int(nullable: false),
+                        ParkingSpot = c.Int(nullable: false),
+                        VehicleTypeId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        MailAddress = c.String(nullable: false, maxLength: 100),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        LastName = c.String(nullable: false, maxLength: 50),
+                        Password = c.String(nullable: false, maxLength: 12),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.VehicleTypes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TypeName = c.String(nullable: false),
+                        ParkingSpace = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.VehicleTypes");
+            DropTable("dbo.People");
+            DropTable("dbo.ParkedVehicles");
+            DropTable("dbo.Colors");
+        }
+    }
+}
